01b5cad8c4fa7aa86e6add5c2a2f17be6efddf3d
macalistervadim <macalistervadim@yandex.ru>
2023-08-20 17:45:33 +0600

    Changes 20.08.2023

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 from .forms import LoginForm, UserRegistrationForm, \
                 UserEditForm, ProfileEditForm
@@ -73,6 +74,11 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated ' \
+                                      'successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
+
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
