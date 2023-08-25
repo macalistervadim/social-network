d3fcc704aa4b98dea07cba378f322bdb4d668604
macalistervadim <macalistervadim@yandex.ru>
2023-08-25 11:22:47 +0600

    Add load picture

--- images/views.py
+++ images/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
+from .models import Image
 from .forms import ImageCreateForm
 
 @login_required
@@ -28,3 +30,10 @@ def image_create(request):
                   'images/image/create.html',
                   {'section': 'images',
                    'form': form})
+
+def image_detail(request, id, slug):
+ image = get_object_or_404(Image, id=id, slug=slug)
+ return render(request,
+               'images/image/detail.html',
+               {'section': 'images',
+                'image': image})
