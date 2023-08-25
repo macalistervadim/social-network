27b4c19f43819050d1273af72b34e7e18d346820
macalistervadim <macalistervadim@yandex.ru>
2023-08-25 11:35:13 +0600

    Add load picture

--- images/views.py
+++ images/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 from .models import Image
 from .forms import ImageCreateForm
@@ -37,3 +39,20 @@ def image_detail(request, id, slug):
                'images/image/detail.html',
                {'section': 'images',
                 'image': image})
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+        return JsonResponse({'status': 'error'})
