75b21c3dc8c97b5cc88bc474abe39430f27ce526
macalistervadim <macalistervadim@yandex.ru>
2023-08-28 11:47:08 +0600

    Changes 28.08.23

--- account/views.py
+++ account/views.py
@@ -1,12 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.views.decorators.http import require_POST
 
 from .forms import LoginForm, UserRegistrationForm, \
                 UserEditForm, ProfileEditForm
-from .models import Profile
+from .models import Profile, Contact
 
 from django.contrib.auth.decorators import login_required
 
@@ -106,3 +107,23 @@ def user_detail(request, username):
     'account/user/detail.html',
         {'section': 'people',
                 'user': user})
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
