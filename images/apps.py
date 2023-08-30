4d09600f612eba0320880875bfd537731141402b
macalistervadim <macalistervadim@yandex.ru>
2023-08-30 11:31:56 +0600

    Add Redis and signals

--- images/apps.py
+++ images/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ImagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'images'
+
+    def ready(self):
+        # импортировать обработчики сигналов
+        import images.signal
